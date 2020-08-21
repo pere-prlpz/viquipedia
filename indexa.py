875a7b0a71b8c0a589a019d060f5fb03fe87cac5
pere-prlpz <prlpzb@gmail.com>
2020-08-21 23:22:00 +0200

    Add files via upload

--- indexa.py
+++ indexa.py
@@ -4,10 +4,12 @@
 # - Nom de la categoria
 # - Part del títol dels articles que s'ha de treure:
 #    - no hi inclogueu "de", "del" i variants, que ja ho fa el programa tot sol
-#    - pot ser una expressió regular (entre parèntesi)
+#    - pot ser una expressió regular
+#    - Si no s'indica aquest paràmetre, fa servir el títol de la categoria
 # Exemples:
 # python indexa.py "Avingudes d'Alacant" "Avinguda"
 # python indexa.py "Arxius municipals del País Valencià" "(Arxiu Municipal|Arxiu Històric Municipal)"
+# python indexa.py "Eleccions del 2016" "(Eleccions ?(parlamentàries|generals|legislatives|regionals|provincials|presidencials|cantonals|federals)?( al)?)"
 
 import re
 import sys
@@ -28,7 +30,10 @@ site=pwb.Site('ca')
 #tcats=u"Plaça" #Poseu aquí la part del títol que no forma part de l'índex (no hi inclogueu "de", "del" i variants, que ja ho fa el programa tot sol).
 arguments = sys.argv[1:]
 tcat = arguments[0]
-tcats = arguments[1]
+if len(arguments) > 1:
+    tcats = arguments[1]
+else:
+    tcats = tcat[:]
 inicial=tcat[0]
 retcat=u"["+inicial.upper()+inicial.lower()+"]"+tcat[1:]
 cat = pwb.Category(site,'Category:'+tcat)
@@ -44,12 +49,15 @@ for pag in pags:
     else:
         ordena = tit
     index=tit
-    index=re.sub("^("+tcat+"|"+tcats+u") (del |de la |de l'|dels |de les )","",index)
-    index=re.sub("^("+tcat+"|"+tcats+u") (de |d')","",index)
-    index=re.sub("^("+tcat+"|"+tcats+u") ","",index)
+    #index=re.sub("^("+tcats+") (del |de la |de l'|dels |de les )","",index)
+    #index=re.sub("^("+tcats+") (de |d')","",index)
+    index=re.sub("^("+tcats+") ","",index)
+    index=re.sub("^(del |de la |de l'|dels |de les )","", index)
+    index=re.sub("^(de |d')","", index)    
+    index=re.sub("^(el |la |l'|els |les )","", index)
     index0=index[:]
-    index=re.sub("[·-]","",index)
-    index=noaccents(index).title()
+    index=re.sub("[·\-\)\(]","",index)
+    index=noaccents(index).title().strip()
     print (index)
     if index != tit and index0 != tit and index != ordena and index0 != ordena and len(index)>0:
         #index=index[0].upper()+index[1:]
