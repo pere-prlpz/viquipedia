6fe182e2d8365194a3974f1f2fe4e2e108016f71
pere-prlpz <prlpzb@gmail.com>
2020-09-08 18:39:16 +0200

    Add files via upload
    Simplificació i millora posició plantilla autoritat

--- autoritat.py
+++ autoritat.py
@@ -24,25 +24,25 @@ def plant(tit, site=pwb.Site('ca')):
 # Retorna el text amb la plantilla (o sense, si no ha trobat on posar-la).
 # És una adaptació de la del programa ccat monuments (l'argument és l'objecte pàgina i no el text).
 #
-def insertaut(page,afegit):
+def insertaut(page,afegit="{{Autoritat}}"):
     if page.isRedirectPage():
         page=page.getRedirectTarget()
     text=page.get()
     if re.search(u"\{\{ ?[Bb]ases de dades taxonòmiques",text):
         text=re.sub(u"\{\{ ?[Bb]ases de dades taxonòmiques",afegit+u"\n{{Bases de dades taxonòmiques",text)
         print (afegit,u"afegit davant de la plantilla Bases de dades taxonòmiques")
-    elif re.search(u"\{\{[Ee]sborrany",text):
-        text=re.sub(u"\{\{[Ee]sborrany",afegit+u"\n{{Esborrany",text,count=1)
-        print (afegit,u"afegit davant de la plantilla esborrany")
-    elif re.search(u"\{\{1000 Biografies",text):
-        text=re.sub(u"\{\{1000 Biografies",afegit+u"\n{{1000 Biografies",text,count=1)
-        print (afegit,u"afegit davant de la plantilla 1000 Biografies")
-    elif re.search(u"\{\{[Ee]nllaç AD",text):
-        text=re.sub(u"\{\{[Ee]nllaç AD",afegit+u"\n{{Enllaç AD",text,count=1)
-        print (afegit,u"afegit davant de la plantilla Enllaç AD")
-    elif re.search(u"\{\{[Ee]nllaç AB",text):
-        text=re.sub(u"\{\{[Ee]nllaç AB",afegit+u"\n{{Enllaç AB",text,count=1)
-        print (afegit,u"afegit davant de la plantilla Enllaç AB")
+#    elif re.search(u"\{\{[Ee]sborrany",text):
+#        text=re.sub(u"\{\{[Ee]sborrany",afegit+u"\n{{Esborrany",text,count=1)
+#        print (afegit,u"afegit davant de la plantilla esborrany")
+#    elif re.search(u"\{\{1000 Biografies",text):
+#        text=re.sub(u"\{\{1000 Biografies",afegit+u"\n{{1000 Biografies",text,count=1)
+#        print (afegit,u"afegit davant de la plantilla 1000 Biografies")
+#    elif re.search(u"\{\{[Ee]nllaç AD",text):
+#        text=re.sub(u"\{\{[Ee]nllaç AD",afegit+u"\n{{Enllaç AD",text,count=1)
+#        print (afegit,u"afegit davant de la plantilla Enllaç AD")
+#    elif re.search(u"\{\{[Ee]nllaç AB",text):
+#        text=re.sub(u"\{\{[Ee]nllaç AB",afegit+u"\n{{Enllaç AB",text,count=1)
+#        print (afegit,u"afegit davant de la plantilla Enllaç AB")
     elif re.search(u"\{\{(ORDENA|DEFAULTSORT)",text):
         text=re.sub(u"\{\{(ORDENA|DEFAULTSORT)",afegit+u"\n\n{{ORDENA",text,count=1)
         print (afegit,u"afegit davant de l'ORDENA")
@@ -88,7 +88,7 @@ def posaplantilles(titol, site=pwb.Site('ca')):
     germans=False
     # Comença la part de posar plantilla Autoritat
     if pag.namespace()!=14 and ((not plant(u'Autoritat') in plantilles) and (not plant(u"Registre d'autoritats") in plantilles) and (not plant(u"Authority control") in plantilles)):
-        textnou=insertaut(pag,u"{{Autoritat}}")
+        textnou=insertaut(pag,"{{Autoritat}}")
         sumari = "Robot posa la plantilla autoritat"
     else:
         #print u"O ja hi ha la plantilla Autoritat o no hi ha cap autoritat a Wikidata"
@@ -107,9 +107,9 @@ def posaplantilles(titol, site=pwb.Site('ca')):
     return()
 
 #el programa comença aquí
-depth = 5 # depth baix per proves
-url1 = "http://petscan.wmflabs.org/?templates_any=&interface_language=en&ns%5B0%5D=1&active_tab=tab_output&edits%5Bbots%5D=both&categories=Principal&templates_no=Autoritat&cb_labels_no_l=1&language=ca&edits%5Bflagged%5D=both&edits%5Banons%5D=both&project=wikipedia&wikidata_prop_item_use=P1296,%20P5513,%20P6412,%20P902,%20P886,P7872,P7357,P2498&search_max_results=500&cb_labels_yes_l=1&cb_labels_any_l=1&depth="
-url2 = "&outlinks_yes=&format=json&doit="
+depth = 15 # depth baix per proves
+url1 = "https://petscan.wmflabs.org/?links_to_all=&project=wikipedia&templates_no=Autoritat&edits%5Banons%5D=both&cb_labels_no_l=1&edits%5Bbots%5D=both&search_max_results=500&ns%5B0%5D=1&cb_labels_yes_l=1&categories=Principal&interface_language=en&larger=70&active_tab=tab_pageprops&language=ca&depth="
+url2 = "&cb_labels_any_l=1&edits%5Bflagged%5D=both&wikidata_prop_item_use=P1296,%20P5513,%20P6412,%20P902,%20P886,P7872,P7357,P2498&format=json&langs_labels_any=&doit="
 url=url1+str(depth)+url2
 titols = llegeix_petscan(url)
 print(titols[0:min(10,len(titols))])
