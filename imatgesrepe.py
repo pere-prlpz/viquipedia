1def1854fe4232f5c98e7f8fe5e2de12b27c7ad3
pere-prlpz <prlpzb@gmail.com>
2021-06-30 19:47:28 +0200

    Add files via upload
    Carregar articles amb generador

--- imatgesrepe.py
+++ imatgesrepe.py
@@ -1,3 +1,13 @@
+# Fa una llista d'articles que tenen dins del viquitext la imatge del P18 de Wikidata.
+# La sortida va a una pàgina d'usuari.
+# Opcionalment, admet com a paràmetre una propietat de Wikidata i aleshores
+# busca només entre els articles amb aquella propietat. Si no, busca entre tots
+# els que tinguin P18.
+# Exemples:
+# python imatgesrepe.py
+# python imatgesrepe.py P625
+# python imatgesrepe.py P21
+
 import pywikibot as pwb
 from pywikibot import pagegenerators
 from SPARQLWrapper import SPARQLWrapper, JSON
@@ -14,15 +24,18 @@ def get_results(endpoint_url, query):
     sparql.setReturnFormat(JSON)
     return sparql.query().convert()
 
-def get_articles():
+def get_articles(prop=""):
     print("Carregant articles i imatges de Wikidata")
+    if prop=="":
+        clausula="\n"
+    else:
+        clausula="\n?subjecte wdt:"+prop+" [].\n"
+    print(clausula)
     query="""#articles amb foto de la infotaula
     SELECT DISTINCT ?article ?imatge WHERE {
-      ?article schema:about ?animal;
+      ?article schema:about ?subjecte;
         schema:isPartOf <https://ca.wikipedia.org/>.
-      ?animal wdt:P18 ?imatge.
-    }"""
-    #alternativa menor Q134681
+      ?subjecte wdt:P18 ?imatge.""" + clausula + "}"
     endpoint_url = "https://query.wikidata.org/sparql"
     results = get_results(endpoint_url, query)
     wd = results["results"]["bindings"]
@@ -30,11 +43,38 @@ def get_articles():
     return (wd)
 
 # el programa comença aquí
+arguments = sys.argv[1:]
+if len(arguments)>0:
+    propietat = arguments[0]
+    explica = " Elements amb {{P|"+propietat+"}} a Wikidata."
+else:
+    propietat = ""
+    explica = ""
+# recollida de dades a Wikidata
 site=pwb.Site('ca')
-repo = site.data_repository()
-dades = get_articles()
+dades = get_articles(prop=propietat)
+print(explica)
 print(dades[0:5])
-titols = [x["article"]["value"] for x in dades]
+# endreça les dades per convertir-les en diccionari
+dicc = {}
+for el in dades:
+    nomart = unquote(el["article"]["value"]).replace("https://ca.wikipedia.org/wiki/","")
+    nomimatge = unquote(el["imatge"]["value"]).replace("http://commons.wikimedia.org/wiki/Special:FilePath/","")
+    pag=pwb.Page(site,nomart)
+    #print(pag)
+    #print(dicc)
+    if pag in dicc:
+        try:
+            #print(pag, dicc[pag])
+            dicc[pag].append(nomimatge)
+            #print(pag, dicc[pag])
+        except AttributeError:
+            print("AttributeError:", pag, dicc[pag])
+            dicc[pag] = [nomimatge]
+    else:
+        dicc[pag] = [nomimatge]
+#print(dicc)
+# busca imatges repetides
 print(len(dades),"imatges o articles trobats")
 total=len(dades)
 compta=0
@@ -44,44 +84,44 @@ pubtrobo=0
 resum = ""
 paginfo=pwb.Page(pwb.Site('ca'),"Usuari:PereBot/imatges repetides")
 resum0 = paginfo.get()
-for el in dades:
-    #print(el)
-    nomart = unquote(el["article"]["value"]).replace("https://ca.wikipedia.org/wiki/","")
-    nomimatge = unquote(el["imatge"]["value"]).replace("http://commons.wikimedia.org/wiki/Special:FilePath/","")
+for art in pagegenerators.PreloadingGenerator(dicc.keys()):
     compta=compta+1
-    pag=pwb.Page(site,nomart)
-    print(compta,"/",total,pag,nomimatge)
+    print(compta,"/",total,art,dicc[art])
     try:
-        text=pag.get()
+        text=art.get()
     except pwb.IsRedirectPage:
         print("Redirecció")
         continue
     except pwb.NoPage:
         print("Pàgina inexistent")
         continue
-    if re.search("[Ii]mat?ge *=.+",text):
+    if re.search("([Ii]mat?ge|[Ff]oto(grafia)?)2? *=.+",text):
         print("té una imatge local a la infotaula")
-    elif nomimatge in text:
-        comptatrobo = comptatrobo+1
-        print("imatge repetida\n********************************")
-        nump18 = titols.count(el["article"]["value"])
-        if nump18 > 1:
-            textdup = f"({nump18} imatges a Wikidata)"
-            print (nump18, "imatges a P18")
-        else:
-            textdup = ""
-        resum = resum + f"# [[{nomart}]], [[:Fitxer:{nomimatge}]] {textdup}\n"
-    if (compta - pubcompta>1000 or comptatrobo - pubtrobo>20) and len(resum)>0:
-        resumpub = resum0+"\n\n== Imatges repetides ==\n\n"
-        resumpub = resumpub+"\nArticles amb la {{P|18}} fora de la infotaula\n\n"+resum
-        resumpub = resumpub+f"Vistos {compta} de {total}.--~~~~"
-        paginfo.put(resumpub, "Robot busca articles amb la imatge de la infotaula repetida")
-        pubcompta=compta
-        pubtrobo=comptatrobo
+        continue
+    for nomimatge in dicc[art]:
+        if re.search("[:\n]"+re.escape(nomimatge), text):
+            comptatrobo = comptatrobo+1
+            print("imatge repetida\n********************************")
+            nump18 = len(dicc[art])
+            if nump18 > 1:
+                textdup = f"({nump18} imatges a Wikidata)"
+                print (nump18, "imatges a P18")
+            else:
+                textdup = ""
+            nomart = art.title()
+            resum = resum + f"# [[{nomart}]], [[:Fitxer:{nomimatge}]] {textdup}\n"
+        if (compta - pubcompta >= 20000 or comptatrobo - pubtrobo >= 400) and len(resum)>0:
+            resumpub = resum0+"\n\n== Imatges repetides ==\n\n"
+            resumpub = resumpub+"\nArticles amb la {{P|18}} fora de la infotaula."+explica+"\n\n"+resum
+            resumpub = resumpub+f"Vistos {compta} de {total}.--~~~~"
+            nores=paginfo.get()
+            paginfo.put(resumpub, "Robot busca articles amb la imatge de la infotaula repetida")
+            pubcompta=compta
+            pubtrobo=comptatrobo
 print(resum)
 if len(resum)>0:
-        resumpub = resum0+"\n\n== Imatges repetides ==\n\n"
-        resumpub = resumpub+"\nArticles amb la {{P|18}} fora de la infotaula\n\n"+resum
-        resumpub = resumpub+f"Vistos {compta} de {total}.--~~~~"    
-        paginfo.put(resumpub, "Robot busca articles amb la imatge de la infotaula repetida")
-    
+    resumpub = resum0+"\n\n== Imatges repetides ==\n\n"
+    resumpub = resumpub+"\nArticles amb la {{P|18}} fora de la infotaula."+explica+"\n\n"+resum
+    resumpub = resumpub+f"Vistos {compta} de {total}.--~~~~"
+    nores=paginfo.get()
+    paginfo.put(resumpub, "Robot busca articles amb la imatge de la infotaula repetida")
